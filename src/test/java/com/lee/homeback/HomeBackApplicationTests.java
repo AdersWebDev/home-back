df37fc38d9836c4760c33e4e50d0250c0bcd362c
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2025-05-09 12:57:38 +0900

    inti

---
+++ src/test/java/com/lee/homeback/HomeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.homeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
